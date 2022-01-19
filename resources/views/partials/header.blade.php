c7b31ce089e49a059dff4d418cd315bfa7b7c60f
pizzi9523 <pizzitolagiuseppe@live.it>
2022-01-19 15:35:14 +0100

    add layouts.app, add header, footer

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,28 @@
+<header id="site_header">
+    <div class="top_header">
+        <div class=" container">
+            <nav class="nav  justify-content-end">
+                <a class="nav-link text-light" href="#">DC POWER℠VISA®</a>
+                <a class="nav-link text-light" href="#">ADDITIONAL DC SITES &dtrif;</a>
+            </nav>
+        </div>
+
+    </div>
+
+    <div class="container  bottom_header d-flex justify-content-between py-4">
+        <div class="logo">
+            <img src="{{asset('img/dc-logo.png')}}" alt="" />
+        </div>
+
+        <nav class="nav bottom_nav justify-content-center align-content-center ">
+            
+            <a class="nav-link active " href="">COMICS </a>
+            
+
+
+            <input id="search_bar" class="text-end ms-5" type="text" placeholder="Search">
+            <label class="d-flex align-items-center" for="search_bar"><i class="fas fa-search"></i></label>
+        </nav>
+
+    </div>
+</header>
