4c0fd8f0bda954aea17cc318a0db6395ae57dd24
pizzi9523 <pizzitolagiuseppe@live.it>
2022-01-19 15:57:39 +0100

    Create the resource route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('comics', 'ComicController@index')->name('comics');
+Route::get('comics/create', 'ComicController@create')->name('comics.create');
+Route::post('comics', 'ComicController@store')->name('comics.store');
+Route::get('comics/{comic}', 'ComicController@show')->name('comic');
+Route::get('comics/{comic}/edit', 'ComicController@edit')->name('comic.edit');
+Route::put('comics/{comic}', 'ComicController@update')->name('comic.update');
+Route::delete('comics/{comic}', 'ComicController@destroy')->name('comic.destroy');
