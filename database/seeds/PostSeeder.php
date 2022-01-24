7eb5813ebf94cef1ab5ace499be1e6357e484d36
pizzi9523 <pizzitolagiuseppe@live.it>
2022-01-24 16:01:58 +0100

    create Post Model, Migration, Seeder, Controller

---
+++ database/seeds/PostSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $post = new Post();
+            $post->title = $faker->sentence();
+            $post->body = $faker->paragraph();
+            $post->save();
+        }
+    }
+}
